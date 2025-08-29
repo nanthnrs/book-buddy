5dd5835f1d829de9d27f4354138e648b61df27e0
Nanthawut Norasing <kernorasing@gmail.com>
2025-08-30 02:47:31 +0700

    feat: load books on app init

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,28 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 import { provideRouter } from '@angular/router';
+import { provideMockStore } from '@ngrx/store/testing';
+import { bookFeatureKey, initialBookState } from './state/book/book.reducer';
+import { Store } from '@ngrx/store';
+import { BookActions } from './state/book/book.actions';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let app: App;
+  let store: Store;
+
+  const initialState = {
+    [bookFeatureKey]: initialBookState
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideRouter([])]
+      providers: [
+        provideRouter([]),
+        provideMockStore({ initialState })
+      ]
     }).compileComponents();
+
+    store = TestBed.inject(Store);
+    spyOn(store, 'dispatch').and.callThrough();
+
+    fixture = TestBed.createComponent(App);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render layout', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('app-header')).toBeTruthy();
     expect(compiled.querySelector('app-navbar')).toBeTruthy();
     expect(compiled.querySelector('app-main')).toBeTruthy();
     expect(compiled.querySelector('app-footer')).toBeTruthy();
+  });
+
+  it('should dispatch loadBooks action on init', () => {
+    fixture.detectChanges();
+    expect(store.dispatch).toHaveBeenCalledWith(BookActions.loadBooks());
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Header } from "./layout/header/header";
 import { Navbar } from "./layout/navbar/navbar";
 import { Main } from "./layout/main/main";
 import { Footer } from "./layout/footer/footer";
+import { Store } from '@ngrx/store';
+import { BookActions } from './state/book/book.actions';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,11 @@ import { Footer } from "./layout/footer/footer";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App { }
+export class App implements OnInit {
+  private store = inject(Store)
+
+  ngOnInit() {
+    console.log('App initialized');
+    this.store.dispatch(BookActions.loadBooks());
+  }
+}
