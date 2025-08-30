242158e63faeb1d14462e150c197838403de7f60
Nanthawut Norasing <kernorasing@gmail.com>
2025-08-30 14:03:32 +0700

    chore: remove unused code

--- src/app/pages/books/books.ts
+++ src/app/pages/books/books.ts
@@ -1,8 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { Book } from '../../core/models/book';
 import { Store } from '@ngrx/store';
 import { selectAllBooks, selectLoadingBooks } from '../../state/book/book.selectors';
-import { BookActions } from '../../state/book/book.actions';
 import { BookList } from "../../components/book-list/book-list";
 
 @Component({
@@ -16,11 +14,4 @@ export class Books {
 
   readonly books = this.store.selectSignal(selectAllBooks);
   readonly loading = this.store.selectSignal(selectLoadingBooks);
-
-  toggleFavorite(book: Book) {
-    this.store.dispatch(BookActions.setFavorite({
-      isbn: book.isbn,
-      isFavorite: !book.isFavorite
-    }));
-  }
 }
