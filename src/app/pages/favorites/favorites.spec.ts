af2fa61a215147b379fb9eb4c38f45c5c4eae48f
Nanthawut Norasing <kernorasing@gmail.com>
2025-08-30 14:02:20 +0700

    feat(favorites): display favorite books

--- src/app/pages/favorites/favorites.spec.ts
+++ src/app/pages/favorites/favorites.spec.ts
@@ -1,23 +1,60 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Favorites } from './favorites';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { selectFavoriteBooks } from '../../state/book/book.selectors';
+import { Book } from '../../core/models/book';
+import { bookFeatureKey, initialBookState } from '../../state/book/book.reducer';
+import { provideRouter } from '@angular/router';
 
 describe('Favorites', () => {
   let component: Favorites;
   let fixture: ComponentFixture<Favorites>;
+  let nativeElement: HTMLElement;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Favorites]
+      imports: [Favorites],
+      providers: [
+        provideMockStore({
+          initialState: {
+            [bookFeatureKey]: initialBookState
+          }
+        }),
+        provideRouter([])
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+
+    store = TestBed.inject(MockStore);
 
     fixture = TestBed.createComponent(Favorites);
     component = fixture.componentInstance;
+    nativeElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render book list component', () => {
+    store.overrideSelector(selectFavoriteBooks, [{ name: 'Book 1', isFavorite: true }] as Book[]);
+    store.refreshState();
+    fixture.detectChanges();
+
+    const bookListComponent = nativeElement.querySelector('app-book-list');
+    expect(bookListComponent).toBeTruthy();
+  });
+
+  it('should display no favorites message when there are no favorite books', () => {
+    store.overrideSelector(selectFavoriteBooks, []);
+    store.refreshState();
+    fixture.detectChanges();
+
+    const noFavoritesElement = nativeElement.querySelector('#no-favorites');
+    expect(noFavoritesElement).toBeTruthy();
+    expect(noFavoritesElement?.textContent).toContain('No favorite books found.');
   });
 });
--- src/app/pages/favorites/favorites.ts
+++ src/app/pages/favorites/favorites.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BookList } from "../../components/book-list/book-list";
+import { Store } from '@ngrx/store';
+import { selectFavoriteBooks } from '../../state/book/book.selectors';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [BookList],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css'
 })
 export class Favorites {
+  private store = inject(Store);
 
+  readonly books = this.store.selectSignal(selectFavoriteBooks);
 }
